02e3ad7a6ec0948208ce78fb0866386563f8f82f
Diego <diego.p.campos@animaeducacao.com.br>
2022-06-30 09:44:26 -0300

    Método completo

---
+++ CapturarIP/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+
+namespace CapturarIP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
+            {
+                Console.WriteLine("Nenhuma rede disponível");
+            }
+
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+
+            var ippaddress = host
+                .AddressList
+                .FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork);
+            Console.WriteLine(ippaddress);
+        }
+    }
+}
